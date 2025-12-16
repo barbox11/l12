5f7e9e9849f9c24e6ce4aa41ca8446c6ef9de367
barbox11 <123384500+barbox11@users.noreply.github.com>
2025-12-16 10:05:53 -0500

    se agregaron todas las rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Página principal ( / )
+Route::get('/', [HomeController::class, 'index']);
+
+// Ruta alternativa /inicio
+Route::get('/inicio', [HomeController::class, 'index']);
+
+
+// Listado principal de productos
+Route::get('/productos', [ProductoController::class, 'index']);
+
+//  Buscador de productos
+Route::get('/productos/buscar/{termino}', [ProductoController::class, 'buscar']);
+
+// Detalle de producto por SKU
+Route::get('/productos/detalle/{sku}', [ProductoController::class, 'detalle']);
+
+//  Cambiar estado de un producto
+Route::get(
+    '/productos/estado/{id}/{estado}',
+    [ProductoController::class, 'cambiarEstado']
+);
+
+// Filtrar productos por estado
+Route::get(
+    '/productos/filtrar/{estado}',
+    [ProductoController::class, 'filtrar']
+);
