b86d7dbfb59bbce09d95348e2a472ab361cb4f76
0Juana0 <juana.torres@unisarc.edu.co>
2025-12-20 09:49:03 -0500

    Comentarios

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -14,12 +14,12 @@ class Producto extends Model
     // Importante: Dile que use la tabla 'productos' (la que ya tienes llena de plantas)
     protected $table = 'productos';
 
-    // Importante: Dile que la clave primaria de la tabla es 'sku'
+    // Importante: Dile que la clave primaria de la tabla es 'sku' para actvar el "deleted_at"
     protected $primaryKey = 'sku';
     public $incrementing = false;
     protected $keyType = 'string'; 
 
-    // Estos son los campos que permitiremos guardar desde el formulario
+    // Estos son los campos que permitiremos guardar desde el formulario para crear un producto
     protected $fillable = [
         'nombre',
         'sku',
